90d8482f49aeccdaf511ff291b3ff44186f3ad1b
Derrick Mehaffy <derrickmehaffy@gmail.com>
2025-03-18 07:25:15 -0700

    init

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,37 @@
+module.exports = [
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "strapi-testing-bucket.s3.us-west-2.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "strapi-testing-bucket.s3.us-west-2.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
---
+++ config/env/production/plugins.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+          region: env("AWS_REGION"),
+          params: {
+            ACL: env("AWS_ACL", "public-read"),
+            Bucket: env("AWS_BUCKET"),
+          },
+        },
+      },
+    },
+  },
+});
